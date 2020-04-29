700419bbcb67feba2f51590ff23293a994fa4738
Xevion <xevioni@yandex.com>
2020-04-29 04:53:18 -0500

    initial commit

---
+++ amend/cli.py
@@ -0,0 +1,4 @@
+"""
+cli.py
+Controls command-line operations and is the primary entry-point to the application.
+"""
---
+++ amend/main.py
@@ -0,0 +1,5 @@
+"""
+main.py
+Main file that organizes most operations.
+Despite the name, the application shouldn't be activated directly here.
+"""
